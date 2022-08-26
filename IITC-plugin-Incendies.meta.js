ad2f9566cc056acf4829e46fe1c3ea6ba45e019d
Samuel <s.thery@agglopole.fr>
2022-08-26 16:30:23 +0200

    v0.0.2 : plus de conflits avec crosslink, plus de dépendances avec d'autres plugins

---
+++ IITC-plugin-Incendies.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         IITC plugin: Incendies
+// @version      0.0.2
+// @namespace https://github.com/s4zm/IITC-incendies
+// @description Affichage des incendies plugin for IITC
+// @author       s4zm
+// @updateURL       https://github.com/s4zm/IITC-incendies/IITC-plugin-Incendies.user.js
+// @downloadURL     https://github.com/s4zm/IITC-incendies/IITC-plugin-Incendies.meta.js
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# IITC-incendies-herault
+# IITC-plugin-Incendies
 plugin IITC pour afficher les interdictions d'accès dans les massifs de l'hérault (risques incendies)
+
+- ajout d'un overlay pour afficher la donnée sur la map
+- ajout d'une fenêtre pour la légende
