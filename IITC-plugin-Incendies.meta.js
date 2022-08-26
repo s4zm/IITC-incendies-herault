db8d0d039590b1d00d2a2502489fc8dcf0165104
Samuel <s.thery@agglopole.fr>
2022-08-26 16:34:18 +0200

    modification url du plugin

--- IITC-plugin-Incendies.meta.js
+++ IITC-plugin-Incendies.meta.js
@@ -4,8 +4,8 @@
 // @namespace https://github.com/s4zm/IITC-incendies
 // @description Affichage des incendies plugin for IITC
 // @author       s4zm
-// @updateURL       https://github.com/s4zm/IITC-incendies/IITC-plugin-Incendies.user.js
-// @downloadURL     https://github.com/s4zm/IITC-incendies/IITC-plugin-Incendies.meta.js
+// @updateURL       https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.user.js
+// @downloadURL     https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.meta.js
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
