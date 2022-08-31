0dc5489ea7f26ec14f5260da9a9c7b220fccb704
S4zm <0ncles4m@gmail.com>
2022-08-31 23:39:34 +0200

    correction bug de masquage du layer lors du décoche/recoche du layer

--- IITC-plugin-Incendies.meta.js
+++ IITC-plugin-Incendies.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         IITC plugin: Incendies
-// @version      0.0.2
-// @namespace https://github.com/s4zm/IITC-incendies
-// @description Affichage des incendies plugin for IITC
+// @name         IITC plugin: Incendies Herault
+// @version      0.0.3
+// @namespace    https://github.com/s4zm/IITC-plugin-Incendies
+// @description  Affichage des restrictions d'accès dans les masifs Héraultais - été 2$
 // @author       s4zm
-// @updateURL       https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.user.js
-// @downloadURL     https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.meta.js
-// @match          https://*.ingress.com/intel*
-// @match          http://*.ingress.com/intel*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @updateURL    https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.u$
+// @downloadURL  https://github.com/s4zm/IITC-plugin-Incendies/IITC-plugin-Incendies.m$
+// @match        https://*.ingress.com/intel*
+// @match        http://*.ingress.com/intel*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAE$
 // @grant        none
 // ==/UserScript==
